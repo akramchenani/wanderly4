70d37dd2da26688e1bf4ec5052db4ff157036260
akramchenani <akramakramchenani19@gmail.com>
2026-04-20 08:08:18 +0100

    Fix DJANGO_SETTINGS_MODULE must be set before Django imports

--- wanderly/asgi.py
+++ wanderly/asgi.py
@@ -1,11 +1,12 @@
 import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wanderly.settings')
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 import notifications.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wanderly.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
